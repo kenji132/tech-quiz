3bf5b4977a3a4c0f2514045664be87233998280a
kenji132 <ue52483@gmail.com>
2020-10-10 15:43:24 +0900

    quiz APIなどの追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Top from './screen/Top';
+import NavBar from './components/NavBar';
+import Quiz from './screen/Quiz';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route path="/quiz" component={Quiz} />
+          <Route render={() => <h4>not found...</h4>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
---
+++ src/screen/Quiz.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Quiz = () => {
+  return (
+    <div>
+		<h2>クイズだよ！</h2>
+		<Link to ='/'>topに戻る</Link>
+    </div>
+  );
+};
+
+export default Quiz;
